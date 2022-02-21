b8fe087ca7f14018e5c6dbf234ab253591cf1012
DaBootO <dariocontrino@yahoo.de>
2022-02-21 11:33:42 +0100

    initial commit with changes to be called inside script

---
+++ src/dataManip/excel_mover.py
@@ -0,0 +1,30 @@
+import openpyxl
+import os
+import string
+import sys
+
+items = 12
+# thereisfaulty = None
+# newline = None
+letters = string.ascii_uppercase
+
+directory = os.path.abspath(os.path.realpath(__file__)[:-len(os.path.basename(__file__))] + "../../") + '/'
+directory_excel = os.path.join(directory, "excel/")
+saveto = os.path.join(directory, "excel/", "finished/")
+for file in os.listdir(directory_excel):
+    # print(file)
+    if file.split('.')[-1] == "xlsx":
+        print(file, "is being processed...")
+        wb = openpyxl.load_workbook(filename=directory_excel+str(file))
+        ws = wb.active
+        rows = len(tuple(ws.rows))
+        columns = len(tuple(ws.columns))
+        pos = str('A') + str(1) + ":" + str(letters[columns+1]) + str(rows)
+        ws.move_range(pos, rows=0, cols=2)
+        ws['A1'] = "0/1"
+        ws['B1'] = "comment"
+        ws.column_dimensions['C'].width = 100.0
+        ws.column_dimensions['D'].width = 100.0
+        if not os.path.exists(saveto):
+                os.mkdir(saveto)
+        wb.save(saveto+str(file))
---
+++ src/dataManip/excelizer.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import os
+import fileinput
+
+items = 12
+directory = os.path.abspath(os.path.realpath(__file__)[:-len(os.path.basename(__file__))] + "../../") + '/'
+for file in os.listdir(directory):
+    if file.endswith(".csv"):
+        f = open(directory+str(file), 'r')
+        lines = f.readlines()
+        f.close()
+        labels = None
+        index = 1
+        lenalllines = len(lines)
+        for line in lines:
+            if len(line.split("|")) == items:
+                labels = line.split("|")
+                break
+            else:
+                index += 1
+        
+        print(file, "has index =", index, "and lenalllines = ", lenalllines)
+        if index >= lenalllines:
+            print("NOT PASSED")
+        elif index < lenalllines:
+            print("#"*5, "PASSED", "#"*5)
+            for n, line in enumerate(fileinput.input(directory+str(file), inplace=True), start=index):
+                if len(line.split('|')) > items:
+                    line = '|'.join(line.split('|')[:items])
+                    print(line)
+                else:
+                    print(line)
+
+            read_file = pd.read_table(directory+str(file), sep="|", names=labels)
+            if not os.path.exists(os.path.join(directory, "excel/")):
+                os.mkdir(os.path.join(directory, "excel/"))
+            read_file.to_excel(directory+"excel/"+file[:-4]+".xlsx", index = None, header=False)

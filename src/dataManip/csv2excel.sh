097277e920d8ab1712577e2401d315c750320264
DaBootO <dariocontrino@yahoo.de>
2022-03-16 23:41:03 +0100

    cleanup code

--- src/dataManip/csv2excel.sh
+++ src/dataManip/csv2excel.sh
@@ -3,7 +3,6 @@ echo "we are taking the output from torscholar and will transform it into the
 0/1 excel scheme for further human analysis..."
 
 SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
 
